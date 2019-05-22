aacc33380e95e8c96ae6fa0b51d733b95001e6f5
angiemugo <angela.mugo@andela.com>
2019-05-22 17:20:03 +0300

    Revert "add forgot password"

---
+++ Kai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kai
+//
+//  Created by Angie Mugo on 16/05/2019.
+//  Copyright © 2019 Angie Mugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Pods/Target Support Files/Pods-Kai/Pods-Kai-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Kai : NSObject
-@end
-@implementation PodsDummy_Pods_Kai
-@end
